075a63918785fc28f60869ac4b15376ff8194216
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-07-03 10:31:29 +0500

    Added Read Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Create from "./components/Create";
-
+import Read from "./components/Read";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Create />} />
+          <Route exact path="/read" element={<Read />} />
         </Routes>
       </BrowserRouter>
     </>
