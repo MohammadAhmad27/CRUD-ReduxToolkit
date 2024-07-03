9932b9909ebead1733a541616f64df73dd038a2f
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-07-03 10:32:22 +0500

    Added Update Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Create from "./components/Create";
 import Read from "./components/Read";
+import Update from "./components/Update";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Create />} />
           <Route exact path="/read" element={<Read />} />
+          <Route exact path="/edit/:id" element={<Update />} />
         </Routes>
       </BrowserRouter>
     </>
